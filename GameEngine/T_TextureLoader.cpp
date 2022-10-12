6007e426c20686fbce29507c920b8732604e9854
Gerard <gerhoquei8@gmail.com>
2022-10-12 19:18:16 +0200

    starting texture loader(nothing work)

---
+++ GameEngine/T_TextureLoader.cpp
@@ -0,0 +1,20 @@
+#include "T_TextureLoader.h"
+
+uint TextureLoader::LoadToMemory(char* buffer, int size, int* width, int* height)
+{
+
+	glBindTexture(GL_TEXTURE_2D, glID);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+
+	ilDeleteImages(1, &imageID);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+
+	return uint();
+}
--- GameEngine/T_TextureLoader.h
+++ GameEngine/T_TextureLoader.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "glew.h"
+
+#include "SDL_opengl.h"
+#include "Primitive.h"
+#include <gl/GL.h>
+#include <gl/GLU.h>
+typedef unsigned int uint;
+
+
+class TextureLoader
+{
+	uint LoadToMemory(char* buffer, int size, int* width = nullptr, int* height = nullptr);
+};
