741d80afb4a6b32ea6f55d4015697f4128dc80eb
Gerard <gerhoquei8@gmail.com>
2022-11-15 09:55:12 +0100

    Update C_Mesh.cpp

--- GameEngine/C_Mesh.cpp
+++ GameEngine/C_Mesh.cpp
@@ -19,12 +19,15 @@ void C_Mesh::InspectorW()
 
 	if (ImGui::CollapsingHeader("Mesh", ImGuiTreeNodeFlags_DefaultOpen)) {
 
+		ImGui::Checkbox("Render Mesh", &gameObject->renderMesh);
+
+
 		ImGui::TextWrapped("Mesh vertices: "); ImGui::SameLine();
 		ImGui::TextColored(ImVec4(255, 255, 0, 255), std::to_string(gameObject->mesh->num_vertices).c_str());
 
 		ImGui::TextWrapped("Mesh indices: "); ImGui::SameLine();
 		ImGui::TextColored(ImVec4(255, 255, 0, 255), std::to_string(gameObject->mesh->num_indices).c_str());
 
-		ImGui::Checkbox("Render Mesh", &gameObject->renderMesh);
+
 	}
 }
